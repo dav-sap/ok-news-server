e4d07dd988cb98ee466a89b1d7c8575ecbadbd75
David Saper <davidsaper2@gmail.com>
2020-04-04 16:35:06 +0300

    build added

---
+++ build/precache-manifest.58cd57f32d47c15f56306b9ad217ce91.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7cf7de050d6bdffc37ec8d25739e2b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41db48af281ddc4768ee",
+    "url": "/static/css/main.b331de88.chunk.css"
+  },
+  {
+    "revision": "18cc022b3c5d4fabcfc8",
+    "url": "/static/js/2.6f7c7e3c.chunk.js"
+  },
+  {
+    "revision": "3b50d53228dfbe177f099ad65b836436",
+    "url": "/static/js/2.6f7c7e3c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41db48af281ddc4768ee",
+    "url": "/static/js/main.9e3296ba.chunk.js"
+  },
+  {
+    "revision": "64381febdf4f8d8e4a5f",
+    "url": "/static/js/runtime-main.58a90282.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "156bbe167d2d854890900b8b5498aac9",
+    "url": "/static/media/oknews.156bbe16.png"
+  }
+]);
